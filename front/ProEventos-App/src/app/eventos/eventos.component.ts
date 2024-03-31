895e3ada2d985079255ebbcefd7cfbb00928f19e
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-03-31 10:54:20 -0300

    Filtrando grid

--- front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -10,13 +10,33 @@ export class EventosComponent implements OnInit {
   larguraImagem: number = 150;
   margemImagem: number = 2;
   mostrarImagem: boolean = false;
-  filtroLista: string = '';
+  private _filtroLista: string = '';
+  public eventosFiltrados: any = [];
+  public eventos: any = [];
+
+  public get filtroLista(): string
+  {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
+  }
+
+  filtrarEventos(filtrarPor: string): any
+  {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    console.log(filtrarPor);
+    return this.eventos.filter(
+      (evento: {tema: string, local: string})=> evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   alterarImagem = () => {
     this.mostrarImagem = !this.mostrarImagem;
   }
-
-  public eventos: any = [];
 
   constructor (private http: HttpClient){
 
@@ -29,7 +49,10 @@ export class EventosComponent implements OnInit {
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos')
     .subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
   }
